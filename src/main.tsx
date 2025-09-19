286299112b193cd14e3e8933202aabbd1f3358dc
gititgoro <admin@behodler.io>
2025-09-19 04:32:04 +0200

    Fix Tailwind CSS v4 configuration - add app.css and update import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import '../app.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
 import { wagmiConfig } from './wagmiConfig'
