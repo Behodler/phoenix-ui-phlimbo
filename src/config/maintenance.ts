ec0c9c8104af34eb3ba2a2f69001e957d3764a28
Justin <12531162+gititGoro@users.noreply.github.com>
2026-03-19 23:56:36 +0200

    maintenance message

--- src/config/maintenance.ts
+++ src/config/maintenance.ts
@@ -4,4 +4,4 @@
  * Set MAINTENANCE_MODE to true to display a full-screen maintenance banner
  * that blocks all interaction with the site. Set to false for normal operation.
  */
-export const MAINTENANCE_MODE = false
+export const MAINTENANCE_MODE = true
