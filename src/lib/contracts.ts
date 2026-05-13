8c1822eb66097a5458c144babe5eeb90a4e91cfc
Justin <12531162+gititGoro@users.noreply.github.com>
2026-05-13 02:43:52 +0200

    whale mint

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -8,14 +8,13 @@ import type { ContractAddresses } from '../types/contracts'
  *            bondingToken is the ERC20 token produced by the bonding curve
  */
 export const MAINNET_CONTRACT_ADDRESSES: ContractAddresses = {
-  //Phase 1 protocol contracts
   PhUSD: "0xf3B5B661b92B75C71fA5Aba8Fd95D7514A9CD605",
 
   // Deployed Phase 2 contracts
   Pauser: "0x7c5A8EeF1d836450C019FB036453ac6eC97885a3",
   PhusdStableMinter: "0x435B0A1884bd0fb5667677C9eb0e59425b1477E5",
   PhlimboEA: "0x3984eBC84d45a889dDAc595d13dc0aC2E54819F4",
-  StableYieldAccumulator: "0xb9639e6Be92033F55E6D9E375Fd1C28ceEdbA50E",
+  StableYieldAccumulator: "0x3bbe928340c61a65cb6c4a87b3fb59b6f3f7606a",
   DepositView: "0x2Fdf77d4Ea75eFd48922B8E521612197FFbB564c",
   YieldStrategyDola: "0xE7aEC21BF6420FF483107adCB9360C4b31d69D78",
   YieldStrategyUSDe: "0xFc629bC5F6339F77635f4F656FBb114A31F7bCB3",
@@ -62,7 +61,7 @@ export const MAINNET_CONTRACT_ADDRESSES: ContractAddresses = {
     BurnerEYE: "0x13fb51bcb3c5ae9e7115730bc1a58ec676ceeef2",
     BurnerSCX: "0xa833603fd82674aec51f8a57c6a27b91bc1725b2",
     BurnerFlax: "0xb63b57025e9bee5bbb66e4a5297ed0ca044d5ff7",
-    BalancerPooler: "0x6e957842afbcd01ce9db296d173f39134b362771",
+    BalancerPooler: "0x4da153dc02bb084528d10335759f2c4447e6f73d",
     GatherWBTC: "0xfd3775f2ccfb94b532b34b2b683e210ba4449880",
   },
 
@@ -70,12 +69,11 @@ export const MAINNET_CONTRACT_ADDRESSES: ContractAddresses = {
   ViewRouter: "0xC17Ce1cE5ebB43fc0cfda9Fe8BbC849c0894631a",
   DepositPageView: "0x50D4443782bB9A6e8D65dAcd593684EDd3FF03b8",
   MintPageView: "0x64FE63ca7BA456a9Bb190140e35DF2e437AbD119",
-
   // NFT staking
   BalancerPoolerMintDebtHook: "0xbe79dc2c302165025166f09193d9905ef262c064",
   NFTStaker: "0xc8514f821a3d801fa8a8c435840a992a4365a13b",
-  BatchNFTMinter: "0xd3104a6e6d53b37061856fe1f31296d8962f9e01",
-  WaUSDC: '0x0'
+  WaUSDC: "0xd4fa2d31b7968e448877f69a96de69f5de8cd23e",
+  BatchNFTMinter: "0x4ef0fde49360ed31c68ed442ff263cc6291041f3",
 };
 
 
